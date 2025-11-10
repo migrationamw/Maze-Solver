c190f6c71b1798e1d04ee072a1e852fbc82f1329
amwolz <wolz.andrew@gmail.com>
2025-11-09 22:14:58 -0700

    final commit

--- mazeSolver.py
+++ mazeSolver.py
@@ -10,8 +10,6 @@ def get_maze(file):
 
 def display_maze(m, path):
     m2 = m[:]
-    os.system('cls') 
-    
     for item in path:
         m2[item[0]][item[1]] = "."
     m2[path[-1][0]][path[-1][1]] = "M"
@@ -23,19 +21,18 @@ def display_maze(m, path):
             item = str(item).replace("1","█")
             item = str(item).replace("2"," ")
             item = str(item).replace("0"," ")
-            
             draw += item
         draw += "\n"
     print(draw)
    
-def move(path):
+def move(path, maze):
     time.sleep(0.3)
     cur = path[-1]
     display_maze(maze,path)
     possibilities = [(cur[0],cur[1] + 1),(cur[0],cur[1]- 1),(cur[0] + 1, cur[1]),(cur[0]-1, cur[1]) ]
-    # to create a different path each time
+
+    # create a different path each time
     random.shuffle(possibilities)
-    
     
     for item in possibilities:
         if item[0] < 0 or item[1] < 0 or item[0] >= len(maze) or item[1] >= len(maze[0]):
@@ -48,23 +45,30 @@ def move(path):
             path = path + (item,)
             display_maze(maze,path)
             input("Solution found! Press enter to finish")
-            os.system('cls') 
             sys.exit()
         else:
             newpath = path + (item,)
-            move(newpath)
+            move(newpath, maze)
             maze[item[0]][item[1]] = "2"
             display_maze(maze,path)
             time.sleep(0.3)
-            
 
+def main():
+    mazesList = []
+    for i in range(len(os.listdir("mazes"))):
+        mazesList.append(i + 1)     
+    print("Choose maze:\n")
+    for m in mazesList:
+        print(f"Maze {m}:")
+        mazeChoice = get_maze(f"mazes\\maze{m}.csv")
+        display_maze(mazeChoice, ((1, 0),))
     
+    while True:
+        choice = int(input("Enter integer maze number to play: "))
+        if 1 <= choice <= len(mazesList):
+            maze = get_maze(f"mazes\\maze{choice}.csv")
+            move(((1,0),), maze)
+        else:
+            print("Invalid Input. Enter an integer between 1 and " + len(mazesList))
 
-# maze = get_maze('maze1.csv')
-maze = get_maze('maze2.csv')
-width = len(maze)
-height = len(maze[0])
-# print(maze)
-
-
-move(((1,0),))
+main()
