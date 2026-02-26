51579a641eb2e767bb382e9d13ac2d83078b1425
opoascii <amwolz@hotmail.com>
2026-02-25 23:27:04 -0700

    added README

---
+++ README.md
@@ -0,0 +1,50 @@
+# Maze Solver
+
+A terminal-based maze solver written in Python. Includes a script with different maze solving algorithms and a folder of mazes represented by CSV files.
+
+## What This Does
+
+**Maze Solver** allows the user to select an input maze to solve with a specified algorithm all through the CLI. After the selection of a maze and algorithm, the script outputs a series of time delayed representations of each step of the chosen solution to that maze. The user can also create new CSV mazes to solve.
+
+### Key Features
+
+- **Breadth First Search** — Extends every path by one square each iteration and skips dead paths.
+- **A\* Breadth First Search** — Extends the path with the lowest total estimated cost from the start square A to the end square B, calculated by the sum of the distance traversed and Manhattan Distance (horizontal distance + vertical distance) to B.
+- **Bidirectional Breadth First Search** — Implements two instances of BFS, one from A and one from B. Iterates upon the instance with the lesser quantity of active paths to save computational cost. 
+- **Depth First Search** — Searches each available path to completion before searching the next available path.
+- **Find total # of solutions** — Implements a brute force DFS approach to find every possible solution to a maze. Prints the total # of solutions to an input maze.
+
+Each algorithm uses a random generator to randomize the solution.
+
+## Requirements
+
+- Python 3.10+
+
+## Installation
+
+```bash
+git clone https://github.com/amwol/Maze-Solver.git
+cd Maze-Solver
+```
+
+## Usage
+
+```bash
+python mazeSolver.py
+```
+
+### Script presents mazes and prompts the user to select a maze.
+
+![alt text](images/image-1.png)
+
+### Script presents algorithms and prompts the user to select an algorithm.
+
+![alt text](images/image-2.png)
+
+### Executes algorithm and illustrates each step
+
+![alt text](images/image-3.png)
+
+## Add Mazes
+
+To add a maze, add the file `maze<N>.csv` to `mazes/` in the same format of 0s, 1s, A and B.
--- mazeSolver.py
+++ mazeSolver.py
@@ -1,74 +1,335 @@
-import csv,os,time,sys,random
+import csv, os, time, random, heapq, math
+from collections import deque
 
-def get_maze(file):
-    f = open(file,'r')
-    reader = csv.reader(f)
-    maze = []
-    for line in reader:
-        maze.append(line)
-    return maze
+class Maze:
 
-def display_maze(m, path):
-    m2 = m[:]
-    for item in path:
-        m2[item[0]][item[1]] = "."
-    m2[path[-1][0]][path[-1][1]] = "M"
-    
-    draw = ""
-    
-    for row in m2:
-        for item in row:
-            item = str(item).replace("1","█")
-            item = str(item).replace("2"," ")
-            item = str(item).replace("0"," ")
-            draw += item
-        draw += "\n"
-    print(draw)
-   
-def move(path, maze):
-    time.sleep(0.3)
-    cur = path[-1]
-    display_maze(maze,path)
-    possibilities = [(cur[0],cur[1] + 1),(cur[0],cur[1]- 1),(cur[0] + 1, cur[1]),(cur[0]-1, cur[1]) ]
+    def __init__(self, mazeChoice: int):
+        self.maze = self.getMaze(os.path.join("mazes", f"maze{mazeChoice}.csv"))
+        self.mazeCopy = []
+        for row in self.maze:
+            self.mazeCopy.append(row[:])
+        self.height = len(self.maze)
+        self.width = len(self.maze[0])
+        self.start = (0, 0)
+        self.end = (0, 0)
+        for i in range(self.height):
+            for j in range(self.width):
+                if self.maze[i][j] == 'A':
+                    self.start = (i, j)
+                elif self.maze[i][j] == 'B':
+                    self.end = (i, j)
+        self.solutions = 0
+        self.found = False
+        self.sleepTime = 0.1
 
-    # create a different path each time
-    random.shuffle(possibilities)
-    
-    for item in possibilities:
-        if item[0] < 0 or item[1] < 0 or item[0] >= len(maze) or item[1] >= len(maze[0]):
-            continue
-        elif maze[item[0]][item[1]] in ["1","2"] :
-            continue
-        elif item in path:
-            continue
-        elif maze[item[0]][item[1]] == "B":
-            path = path + (item,)
-            display_maze(maze,path)
-            input("Solution found! Press enter to finish")
-            sys.exit()
-        else:
-            newpath = path + (item,)
-            move(newpath, maze)
-            maze[item[0]][item[1]] = "2"
-            display_maze(maze,path)
-            time.sleep(0.3)
+    def getMaze(self, file: str) -> list[list[int]]:
+        with open(file, 'r') as f:  
+            reader = csv.reader(f)
+            maze = []
+            for line in reader:
+                maze.append(line)
+            return maze
+
+    def displayMaze(self, path: tuple[tuple[int, int]] = ()) -> None:
+        m2 = []
+        for row in self.maze:
+            m2.append(row[:])
+
+        # draw path
+        if path:
+            for p in path:
+                m2[p[0]][p[1]] = '.'
+            m2[path[-1][0]][path[-1][1]] = 'X'
+        
+        # draw A and B
+        m2[self.start[0]][self.start[1]] = 'A'
+        m2[self.end[0]][self.end[1]] = 'B'
+
+        # draw maze
+        draw = ""
+        for row in m2:
+            for item in row:
+                item = str(item).replace('1','█')
+                item = str(item).replace('2',' ')
+                item = str(item).replace('0',' ')
+                draw += item
+            draw += "\n"
+        print(draw)
+
+    def displayMaze2(self, path: tuple[tuple[int, int]] = (), end: bool = False) -> None:
+        m2 = []
+        for row in self.maze:
+            m2.append(row[:])
+
+        # draw pathF
+        if path:
+            for p in path:
+                m2[p[0]][p[1]] = '.'
+            m2[path[-1][0]][path[-1][1]] = 'X'
+        if end:
+            m2[path[-1][0]][path[-1][1]] = '.'
+
+        # draw A and B
+        m2[self.start[0]][self.start[1]] = 'A'
+        m2[self.end[0]][self.end[1]] = 'B'
+
+        # draw maze
+        draw = ""
+        for row in m2:
+            for item in row:
+                item = str(item).replace('1','█')
+                item = str(item).replace('2',' ')
+                item = str(item).replace('3',' ')
+                item = str(item).replace('0',' ')
+                draw += item
+            draw += "\n"
+        print(draw)
+
+    def bfsSolve(self) -> None:
+        self.maze[self.start[0]][self.start[1]] = '2'
+        queue = deque([(self.start, )])
+        while queue:
+            time.sleep(self.sleepTime)
+            currPath = queue.popleft()
+            currLocation = currPath[-1]
+            self.displayMaze(currPath)
+            if currLocation == self.end:
+                self.maze = self.mazeCopy
+                print("Solution found!\n")
+                return
+            nbrs = [(currLocation[0], currLocation[1] + 1), (currLocation[0] - 1, currLocation[1]), 
+                    (currLocation[0], currLocation[1] - 1), (currLocation[0] + 1, currLocation[1])]
+            random.shuffle(nbrs)
+            for nbr in nbrs:
+                if min(nbr) < 0 or nbr[0] == self.height or nbr[1] == self.width:
+                    continue
+                elif self.maze[nbr[0]][nbr[1]] in {'1', '2'}:
+                    continue
+                self.maze[nbr[0]][nbr[1]] = '2'
+                queue.append(currPath + (nbr, ))
+        self.maze = self.mazeCopy
+
+    def aStar(self) -> None:
+        self.maze[self.start[0]][self.start[1]] = '2'
+        queue = []
+        heapq.heappush(queue, (math.sqrt((self.end[0] - self.start[0]) ** 2 + (self.end[1] - self.start[1]) ** 2), (self.start, )))
+        while queue:
+            time.sleep(self.sleepTime)
+            currPath = heapq.heappop(queue)[1]
+            currLocation = currPath[-1]
+            self.displayMaze(currPath)
+            if currLocation == self.end:
+                self.maze = self.mazeCopy
+                print("Solution found!\n")
+                return
+            nbrs = [(currLocation[0], currLocation[1] + 1), (currLocation[0] - 1, currLocation[1]), 
+                    (currLocation[0], currLocation[1] - 1), (currLocation[0] + 1, currLocation[1])]
+            random.shuffle(nbrs)
+            for nbr in nbrs:
+                if min(nbr) < 0 or nbr[0] == self.height or nbr[1] == self.width:
+                    continue
+                elif self.maze[nbr[0]][nbr[1]] in {'1', '2'}:
+                    continue
+                self.maze[nbr[0]][nbr[1]] = '2'
+                traveledDistance = len(currPath) - 1
+                manhattanDistance = abs(self.end[0] - nbr[0]) + abs(self.end[1] - nbr[1])
+                euclideanDistance = math.sqrt((self.end[0] - nbr[0]) ** 2 + (self.end[1] - nbr[1]) ** 2)
+                heapq.heappush(queue, (1 + traveledDistance + manhattanDistance, currPath + (nbr, )))
+        self.maze = self.mazeCopy
+
+    def bidirectionalBFS(self) -> None:
+        currStart = [(self.start, )]
+        currEnd = [(self.end, )]
+        while currStart and currEnd:
+            if len(currStart) <= len(currEnd):
+                nextStart = []
+                for startPath in currStart:
+                    time.sleep(self.sleepTime)
+                    self.displayMaze2(startPath)
+                    currStartLocation = startPath[-1]
+                    if self.maze[currStartLocation[0]][currStartLocation[1]] == '3':
+                        for endPath in currEnd:
+                            if endPath[-2] == currStartLocation:
+                                time.sleep(self.sleepTime)
+                                self.displayMaze2(startPath + endPath[:-1])
+                                time.sleep(self.sleepTime)
+                                self.displayMaze2(startPath + endPath[:-1], True)
+                                self.maze = self.mazeCopy
+                                return
+                    self.maze[currStartLocation[0]][currStartLocation[1]] = '2'
+                    nbrs = [(currStartLocation[0], currStartLocation[1] + 1), (currStartLocation[0] - 1, currStartLocation[1]), 
+                            (currStartLocation[0], currStartLocation[1] - 1), (currStartLocation[0] + 1, currStartLocation[1])]
+                    random.shuffle(nbrs)
+                    for nbr in nbrs:
+                        if min(nbr) < 0 or nbr[0] == self.height or nbr[1] == self.width:
+                            continue
+                        elif self.maze[nbr[0]][nbr[1]] in {'1', '2'}:
+                            continue
+                        contains = 0
+                        for nextStartPath in nextStart:
+                            if nextStartPath[-1] == nbr:
+                                contains += 1
+                                break
+                        if contains == 0:
+                            nextStart.append(startPath + (nbr, ))
+                currStart = nextStart
+            else:
+                nextEnd = []
+                for endPath in currEnd:
+                    time.sleep(self.sleepTime)
+                    self.displayMaze2(endPath)
+                    currEndLocation = endPath[-1]
+                    if self.maze[currEndLocation[0]][currEndLocation[1]] == '2':
+                        for startPath in currStart:
+                            if startPath[-2] == currEndLocation:
+                                time.sleep(self.sleepTime)
+                                self.displayMaze2(startPath[:-1] + endPath)
+                                time.sleep(self.sleepTime)
+                                self.displayMaze2(startPath[:-1] + endPath, True)
+                                self.maze = self.mazeCopy
+                                return
+                    self.maze[currEndLocation[0]][currEndLocation[1]] = '3'
+                    nbrs = [(currEndLocation[0], currEndLocation[1] + 1), (currEndLocation[0] - 1, currEndLocation[1]), 
+                            (currEndLocation[0], currEndLocation[1] - 1), (currEndLocation[0] + 1, currEndLocation[1])]
+                    random.shuffle(nbrs)
+                    for nbr in nbrs:
+                        if min(nbr) < 0 or nbr[0] == self.height or nbr[1] == self.width:
+                            continue
+                        elif self.maze[nbr[0]][nbr[1]] in {'1', '3'}:
+                            continue
+                        contains = 0
+                        for nextEndPath in nextEnd:
+                            if nextEndPath[-1] == nbr:
+                                contains += 1
+                                break
+                        if contains == 0:
+                            nextEnd.append(endPath + (nbr, ))
+                currEnd = nextEnd
+
+    def dfsSolve(self, path: tuple[tuple[int, int], ...]) -> None:
+        if self.found:
+            return
+        elif not path:
+            path = (self.start, )
+            self.maze[self.start[0]][self.start[1]] = '2'
+        self.displayMaze(path)
+        curr = path[-1]
+        if curr == self.end:
+            self.maze = self.mazeCopy
+            self.found = True
+            print("Solution found!\n")
+            return
+        nbrs = [(curr[0], curr[1] + 1), (curr[0] - 1, curr[1]), 
+                (curr[0], curr[1] - 1), (curr[0] + 1, curr[1])]
+        random.shuffle(nbrs)
+        first = 0
+        for nbr in nbrs:
+            if min(nbr) < 0 or nbr[0] == self.height or nbr[1] == self.width:
+                continue
+            elif self.maze[nbr[0]][nbr[1]] in {'1', '2'}:
+                continue
+            if first == 0:
+                time.sleep(self.sleepTime)
+                first += 1
+            self.maze[nbr[0]][nbr[1]] = '2'
+            self.dfsSolve(path + (nbr, ))
+            if self.found:
+                return
+            self.displayMaze(path)
+            time.sleep(self.sleepTime)
+        if first == 0:
+            time.sleep(self.sleepTime)
+
+    def findTotalSolutions(self, path: tuple[tuple[int, int], ...]) -> None:
+        if not path:
+            path = (self.start, )
+            self.maze[self.start[0]][self.start[1]] = '2'
+            self.solutions = 0
+        self.displayMaze(path)
+        curr = path[-1]
+        if curr == self.end:
+            self.solutions += 1
+            print("Solution Found! Total # of solutions: " + str(self.solutions) + "\n")
+            # reset maze visited and not in path
+            self.maze = self.mazeCopy
+            for i, j in path:
+                self.maze[i][j] = '2'
+            time.sleep(self.sleepTime)
+            return
+        nbrs = [(curr[0], curr[1] + 1), (curr[0] - 1, curr[1]), 
+                (curr[0], curr[1] - 1), (curr[0] + 1, curr[1])]
+        random.shuffle(nbrs)
+        first = 0
+        for nbr in nbrs:
+            if min(nbr) < 0 or nbr[0] == self.height or nbr[1] == self.width:
+                continue
+            elif self.maze[nbr[0]][nbr[1]] in {'1', '2'}:
+                continue
+            if first == 0:
+                time.sleep(self.sleepTime)
+                first += 1
+            self.maze[nbr[0]][nbr[1]] = '2'
+            self.findTotalSolutions(path + (nbr, ))
+            self.maze[nbr[0]][nbr[1]] = '0'
+            self.displayMaze(path)
+            time.sleep(self.sleepTime)
+        if first == 0:
+            time.sleep(self.sleepTime)
 
 def main():
-    mazesList = []
-    for i in range(len(os.listdir("mazes"))):
-        mazesList.append(i + 1)     
-    print("Choose maze:\n")
-    for m in mazesList:
-        print(f"Maze {m}:")
-        mazeChoice = get_maze(f"mazes\\maze{m}.csv")
-        display_maze(mazeChoice, ((1, 0),))
-    
+    numMazes = len(os.listdir("mazes"))
     while True:
-        choice = int(input("Enter integer maze number to play: "))
-        if 1 <= choice <= len(mazesList):
-            maze = get_maze(f"mazes\\maze{choice}.csv")
-            move(((1,0),), maze)
+        print("Choose maze:\n")
+        mazes = []
+        for i in range(1, numMazes + 1):
+            print(f"Maze {i}:")
+            currMaze = Maze(i)
+            currMaze.displayMaze()
+            mazes.append(currMaze)
+        # choose maze
+        try:
+            choice = int(input(f"Enter maze selection [1-{numMazes}]: "))
+        except KeyboardInterrupt:
+            print("\nProgram terminated by user\n")
+            break
+        except:
+            print("Invalid Input. Enter an integer between 1 and " + str(numMazes) + "\n")
+            continue
+        
+        if 1 <= choice <= numMazes:
+            chosenMaze = mazes[choice - 1]
+            print("")
+            chosenMaze.displayMaze()
+            print("How would you like to solve?")
+            print("1. Breadth First Search")
+            print("2. A* Breadth First Search")
+            print("3. Bidirectional Breadth First Search")            
+            print("4. Depth First Search")            
+            print("5. Find total # of solutions")
+            print("")            
+
+            solve = input("Enter algorithm selection [1-5]: ")
+            # choose to fully solve or find a solution
+            if solve == '1':
+                chosenMaze.bfsSolve()
+                chosenMaze.maze = chosenMaze.mazeCopy
+            elif solve == '2':
+                chosenMaze.aStar()
+                chosenMaze.maze = chosenMaze.mazeCopy
+            elif solve == '3':
+                chosenMaze.bidirectionalBFS()
+                chosenMaze.maze = chosenMaze.mazeCopy
+            elif solve == '4':
+                chosenMaze.dfsSolve(())
+                chosenMaze.found = False
+                chosenMaze.maze = chosenMaze.mazeCopy
+            elif solve == '5':
+                chosenMaze.findTotalSolutions(())
+                chosenMaze.maze = chosenMaze.mazeCopy
+                print("Total possible solutions = " + str(chosenMaze.solutions))
+            else:
+                print("Invalid Input. Enter an integer between 1 and 5\n")
         else:
-            print("Invalid Input. Enter an integer between 1 and " + len(mazesList))
+            print("Invalid Input. Enter an integer between 1 and " + str(numMazes) + "\n")
 
 main()
